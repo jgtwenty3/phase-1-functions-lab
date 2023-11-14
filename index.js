eeb4f484b30bfab577a34889fd4f15e77eeac9e7
Justin <justinguerrerowork@gmail.com>
2023-11-14 14:11:05 -0500

    need to get better at reading tests and errors

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    const headquartersLocation = 42; 
+    const distance = Math.abs(location - headquartersLocation);
+    return distance;
+}
+function distanceFromHqInFeet(location){
+    const blockInFeet = 264;
+    const distanceInBlocks = Math.abs(location-42);
+    const distanceInFeet = Math.abs(distanceInBlocks * blockInFeet);
+    return distanceInFeet;
+}
+function distanceTravelledInFeet(start, destination) {
+    const blocksInFeet = 264;
+    const distanceInBlocks = Math.abs(destination - start);
+    const distanceInFeet = Math.abs(distanceInBlocks * blocksInFeet);
+    return distanceInFeet;
+}
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
